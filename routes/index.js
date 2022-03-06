bb7f52d2009d3b2d8aec8f259f1d6ab9c814f867
Q1one <josepgamer76@gmail.com>
2022-03-05 22:19:55 -0500

    mod css

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Yoimiya Bot | Bot Anti-Scam' });
+  res.render('index', { title: 'BOT | Bot Anti-Scam' });
 });
 
 router.get('/donate', (req, res) => {
-  res.render('donate', { title: 'Yoimiya Bot | Donate' });
+  res.render('donate', { title: 'BOT | Donate' });
 });
 
 router.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Yoimiya Bot | Contact' });
+  res.render('contact', { title: 'BOT | Contact' });
 });
 
 router.get('/docs', (req, res) => {
-  res.render('docs', { title: 'Yoimiya Bot | Documents' });
+  res.render('docs', { title: 'BOT | Documents' });
 });
 
 
